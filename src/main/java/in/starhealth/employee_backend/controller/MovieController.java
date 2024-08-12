7a45971c0051da588b35068193e8a45a1f079c0c
suvarna.bhide <suvarna.bhide@starhealth.in>
2024-08-12 14:36:53 +0530

    eliminated code redundancy

--- src/main/java/in/starhealth/employee_backend/controller/MovieController.java
+++ src/main/java/in/starhealth/employee_backend/controller/MovieController.java
@@ -1,18 +1,11 @@
 package in.starhealth.employee_backend.controller;
 
-import in.starhealth.employee_backend.exception.ResourceNotFoundException;
-import in.starhealth.employee_backend.model.Employee;
-import in.starhealth.employee_backend.repository.EmployeeRepository;
-import in.starhealth.employee_backend.service.EmployeeService;
 import in.starhealth.employee_backend.service.MovieService;
-import in.starhealth.employee_backend.service.StudentService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.*;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.client.RestTemplate;
 
 import java.io.IOException;
-import java.util.List;
 
 @RestController
 @RequestMapping("movies")
